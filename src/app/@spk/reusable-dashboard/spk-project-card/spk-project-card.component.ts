d9adce6da27f7eb089d5629a5810efa3c04f5bb5
Sravan  - xAmplify <sravan@xamplify.com>
2025-08-09 11:35:24 +0530

    first commit

---
+++ src/app/@spk/reusable-dashboard/spk-project-card/spk-project-card.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'spk-project-card',
+    imports: [CommonModule],
+    templateUrl: './spk-project-card.component.html',
+    styleUrl: './spk-project-card.component.scss'
+})
+export class SpkProjectCardComponent {
+  @Input() tasks: Array<{
+    label: string;
+    count: number;
+    percentage: string;
+    percentageClass: string;
+    itemClass: string;
+    iconClass: string;
+  }> = [];
+}
---
+++ src/app/@spk/reusable-ui-elements/spk-badges/spk-badges.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'spk-badges',
+    imports: [],
+    templateUrl: './spk-badges.component.html',
+    styleUrl: './spk-badges.component.scss'
+})
+export class SpkBadgesComponent {
+@Input() class:string='';
+@Input() variant:string='';
+
+}
